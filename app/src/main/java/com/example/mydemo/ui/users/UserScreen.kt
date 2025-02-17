d5a07b8ddaab4c835d410d2a75bb1f9cac6f3aaa
Jasper Haegeman <jasper.haegeman@skynet.be>
2025-02-17 14:03:10 +0100

    basic initial screens for home and user navigation

---
+++ app/src/main/java/com/example/mydemo/ui/users/UserScreen.kt
@@ -0,0 +1,26 @@
+package com.example.mydemo.ui.users
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Surface(color = Color.Black) {
+        Text(
+            text = "This will be our user overview!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
