b42422dd6eb5a305d741087758a13b9743fde032
jasper haegeman <jasper.haegeman@skynet.be>
2025-02-18 16:36:47 +0100

    base64 string in res/raw folder and extended the helper

--- app/src/main/java/com/example/mydemo/helpers/decodeBase64ToBitmap.kt
+++ app/src/main/java/com/example/mydemo/helpers/decodeBase64ToBitmap.kt
@@ -1,5 +1,6 @@
 package com.example.mydemo.helpers
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.util.Base64
 import java.io.ByteArrayInputStream
@@ -9,6 +10,15 @@ fun decodeBase64ToBitmap(base64String: String): Bitmap? {
     return try {
         val decodedString = Base64.decode(base64String, Base64.DEFAULT)
         BitmapFactory.decodeStream(ByteArrayInputStream(decodedString))
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
+
+fun Context.readBase64FromRaw(resourceId: Int): String? {
+    return try {
+        resources.openRawResource(resourceId).bufferedReader().use { it.readText() }
     } catch (e: Exception) {
         e.printStackTrace()
         null
