efe751dd2e42d49633bdf041c7dd408370b1da85
Arno Heekhout <83210630+arnoheekhout@users.noreply.github.com>
2025-02-18 11:11:20 +0100

    Flavor color theme

--- app/src/main/java/com/example/mydemo/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/mydemo/ui/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.example.mydemo.ui.home
 
 import android.provider.Settings.Global.getString
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,26 +12,33 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mydemo.R
 import androidx.compose.ui.res.stringResource
+import com.example.mydemo.ui.theme.DemoTheme
 
 @Composable
 fun HomeScreen(
     onNextButtonClicked: (Int) -> Unit,
-    modifier: Modifier = Modifier) {
-
-        val appName = stringResource(R.string.app_name)
-        Surface(color = Color.Red) {
-            Text(
-                text = "Welcome to $appName",
-                modifier = modifier.padding(24.dp)
-            )
-        }
+    modifier: Modifier = Modifier
+) {
+    val appName = stringResource(R.string.app_name)
+    Surface(
+        color = MaterialTheme.colorScheme.surface,
+        contentColor = MaterialTheme.colorScheme.onSurface
+    ) {
+        Text(
+            text = "Welcome to $appName",
+            modifier = modifier.padding(24.dp),
+            color = MaterialTheme.colorScheme.onSurface
+        )
     }
+}
 
 @Preview
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen(
-        onNextButtonClicked = TODO(),
-        modifier = Modifier
-    )
+    DemoTheme {
+        HomeScreen(
+            onNextButtonClicked = TODO(),
+            modifier = Modifier
+        )
+    }
 }
