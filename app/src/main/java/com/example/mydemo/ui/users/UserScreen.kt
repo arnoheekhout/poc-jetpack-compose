3ea93842ae6ddf3342de27a354a9789d9d706284
Arno Heekhout <83210630+arnoheekhout@users.noreply.github.com>
2025-02-18 13:25:17 +0100

    inladen gebruiker werkt

--- app/src/main/java/com/example/mydemo/ui/users/UserScreen.kt
+++ app/src/main/java/com/example/mydemo/ui/users/UserScreen.kt
@@ -1,18 +1,28 @@
 package com.example.mydemo.ui.users
 
+import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun UserScreen(
     onNextButtonClicked: (Int) -> Unit,
-    modifier: Modifier = Modifier) {
+    modifier: Modifier = Modifier,
+    viewModel: UserScreenViewModel = viewModel()) {
+
+    val currentUser by viewModel.uiState.collectAsState()
+
+    Log.d("UserScreen", "Current user: $currentUser")
+
     Surface(color = MaterialTheme.colorScheme.primary) {
         Text(
             text = "This will be our user overview!",
