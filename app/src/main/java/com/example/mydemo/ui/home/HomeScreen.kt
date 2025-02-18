f0bdf72e6e986da3a3cdd66a941833dfa95c1c63
Arno Heekhout <83210630+arnoheekhout@users.noreply.github.com>
2025-02-18 09:30:03 +0100

    Toevoegen verschillende strings + homescreen test

--- app/src/main/java/com/example/mydemo/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/mydemo/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.mydemo.ui.home
 
+import android.provider.Settings.Global.getString
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -8,14 +9,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.mydemo.R
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun HomeScreen(
     onNextButtonClicked: (Int) -> Unit,
     modifier: Modifier = Modifier) {
+
+        val appName = stringResource(R.string.app_name)
         Surface(color = Color.Red) {
             Text(
-                text = "This will be our homepage!",
+                text = "Welcome to $appName",
                 modifier = modifier.padding(24.dp)
             )
         }
@@ -26,6 +31,6 @@ fun HomeScreen(
 fun HomeScreenPreview() {
     HomeScreen(
         onNextButtonClicked = TODO(),
-        modifier = TODO()
+        modifier = Modifier
     )
 }
