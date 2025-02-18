dce7a7ef1efc5460a3942bf2884f78a6f0e37eb0
Arno Heekhout <83210630+arnoheekhout@users.noreply.github.com>
2025-02-18 15:58:24 +0100

    Start van coil, werkt niet

--- app/src/main/java/com/example/mydemo/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/mydemo/ui/home/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.example.mydemo.ui.home
 
 import android.provider.Settings.Global.getString
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -12,6 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mydemo.R
 import androidx.compose.ui.res.stringResource
+import coil3.compose.AsyncImage
 import com.example.mydemo.ui.theme.DemoTheme
 
 @Composable
@@ -24,11 +27,22 @@ fun HomeScreen(
         color = MaterialTheme.colorScheme.surface,
         contentColor = MaterialTheme.colorScheme.onSurface
     ) {
-        Text(
-            text = "Welcome to $appName",
-            modifier = modifier.padding(24.dp),
-            color = MaterialTheme.colorScheme.onSurface
-        )
+        Column(  // Added Column to arrange elements vertically
+            modifier = modifier.padding(24.dp)
+        ) {
+            Text(
+                text = "Welcome to $appName",
+                color = MaterialTheme.colorScheme.onSurface
+            )
+
+            AsyncImage(
+                model = "https://bakeronline.be/assets/images/bakeronline/logo-header-small.svg",
+                contentDescription = "Bakeronline logo",
+                modifier = Modifier
+                    .padding(top = 16.dp)
+                    .size(200.dp) 
+            )
+        }
     }
 }
 
